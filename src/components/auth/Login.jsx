7cca79b45e508d9d6b2c9edd96c794128cdc56e0
Jeremy Gitau <jeremyhizasi@gmail.com>
2026-01-07 17:39:34 +0300

    testing on removing api proxy on url endpoints

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
 
     try {
       setSubmitting(true);
-      const response = await fetch("/api/login", {
+      const response = await fetch("/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
