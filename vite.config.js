d706094b3eff6111cbbc0c43c0b5438720e1aaac
Jeremy Gitau <jeremyhizasi@gmail.com>
2026-01-07 18:00:30 +0300

    update on vite.config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       
       '/api': {
-        target: 'https://bookstore-backend-0htz.onrender.com/', 
+        target: 'https://bookstore-backend-0htz.onrender.com', 
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''), 
       },
